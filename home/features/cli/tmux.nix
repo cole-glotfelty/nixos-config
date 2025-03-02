b26a0a7e0b4724a22a63d747a9c2198e64472cfa
Cole Glotfelty <git@postagepaid.cc>
2025-03-02 02:20:00 -0500

    129 current  2025-03-02 02:19:51  25.05.20250227.6313551  6.13.4-zen1                          *

--- home/features/cli/tmux.nix
+++ home/features/cli/tmux.nix
@@ -18,6 +18,8 @@ in {
         set -ga terminal-overrides ",screen-256color*:Tc"
         set -g status-style 'bg=#333333 fg=#5eacd3'
 
+        set-option -g update-environment "PROJECT_DIRS"
+
         bind -T copy-mode-vi v send-keys -X begin-selection
         bind -T copy-mode-vi y send-keys -X copy-pipe-and-cancel 'wl-copy'
         bind-key -r f run-shell "tmux neww ${pkgs.tmux-sessionizer}/bin/tmux-sessionizer"
